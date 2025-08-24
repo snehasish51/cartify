5169a44915a83982c502425d16c15479e465512f
snehasish51 <snehasishg029@gmail.com>
2025-08-24 16:24:44 +0530

    Initial commit

--- App.tsx
+++ App.tsx
@@ -76,6 +76,7 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+            <Text>Cartify</Text>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
